985fb4f64cc291226eea67bd1558d250189ce718
Animesh <rajuranjanxbkj@gmail.com>
2026-09-12 15:09:16 +0530

    Feature: Prioritize Launch Code images in homepage slideshow and add Launch Code category to Gallery

--- client/src/data/gallery.js
+++ client/src/data/gallery.js
@@ -1,5 +1,7 @@
 export const DRIVE_LINKS = {
   All: 'https://drive.google.com/drive/folders/1CiJmBmCJD0b_Ns36vb2VOCT4JkfkHW49?usp=sharing',
+  'Launch Code':
+    'https://drive.google.com/drive/folders/1CiJmBmCJD0b_Ns36vb2VOCT4JkfkHW49?usp=sharing',
   'Orientation 2026':
     'https://drive.google.com/drive/folders/1oACctYp5rEj5WXfbZEXErT-h7eCSB7JM?usp=drive_link',
   'TechFusion 2025':
@@ -9,6 +11,7 @@ export const DRIVE_LINKS = {
 };
 
 export const carouselImages = [
+  { src: '/images/galleryHighlights/IMG_20260829_141921.webp', alt: 'Launch Code 2026' },
   { src: '/images/galleryHighlights/ori-05.webp', alt: 'Orientation 2026' },
   { src: '/images/galleryHighlights/tf-01.webp', alt: 'TechFusion 2025' },
   { src: '/images/galleryHighlights/ori-07.webp', alt: 'Orientation 2026' },
@@ -19,18 +22,30 @@ export const carouselImages = [
 ];
 
 export const galleryImages = [
+  // Launch Code 2026
+  { src: '/images/galleryHighlights/IMG_20260829_141921.webp', alt: 'Launch Code Contest', event: 'Launch Code' },
+  { src: '/images/galleryHighlights/IMG-20260829-WA0011.webp', alt: 'Launch Code Participants', event: 'Launch Code' },
+  { src: '/images/galleryHighlights/IMG-20260829-WA0012.webp', alt: 'Launch Code Live Arena', event: 'Launch Code' },
+  { src: '/images/gallery/IMG_20260905_142230.webp', alt: 'Launch Code Session', event: 'Launch Code' },
+  { src: '/images/gallery/IMG_20260829_142534.webp', alt: 'Launch Code Problem Solving', event: 'Launch Code' },
+
+  // Orientation 2026
   { src: '/images/galleryHighlights/ori-03.webp', alt: 'Orientation 2026', event: 'Orientation 2026' },
   { src: '/images/galleryHighlights/ori-05.webp', alt: 'Orientation 2026', event: 'Orientation 2026' },
   { src: '/images/galleryHighlights/ori-07.webp', alt: 'Orientation 2026', event: 'Orientation 2026' },
   { src: '/images/galleryHighlights/ori-08.webp', alt: 'Orientation 2026', event: 'Orientation 2026' },
   { src: '/images/galleryHighlights/ori-09.webp', alt: 'Orientation 2026', event: 'Orientation 2026' },
   { src: '/images/galleryHighlights/ori-06.webp', alt: 'Orientation 2026', event: 'Orientation 2026' },
+
+  // TechFusion 2025
   { src: '/images/galleryHighlights/tf-01.webp', alt: 'TechFusion 2025', event: 'TechFusion 2025' },
   { src: '/images/galleryHighlights/tf-03.webp', alt: 'TechFusion 2025', event: 'TechFusion 2025' },
   { src: '/images/galleryHighlights/tf-04.webp', alt: 'TechFusion 2025', event: 'TechFusion 2025' },
   { src: '/images/galleryHighlights/tf-05.webp', alt: 'TechFusion 2025', event: 'TechFusion 2025' },
   { src: '/images/galleryHighlights/tf-06.webp', alt: 'TechFusion 2025', event: 'TechFusion 2025' },
   { src: '/images/galleryHighlights/tf-02.webp', alt: 'TechFusion 2025', event: 'TechFusion 2025' },
+
+  // Club moments
   { src: '/images/gallery2/1.webp', alt: 'Club moments', event: 'Club moments' },
   { src: '/images/gallery2/2.webp', alt: 'Club moments', event: 'Club moments' },
   { src: '/images/gallery2/3.webp', alt: 'Club moments', event: 'Club moments' },
@@ -39,4 +54,5 @@ export const galleryImages = [
   { src: '/images/gallery2/6.webp', alt: 'Club moments', event: 'Club moments' },
 ];
 
-export const galleryEvents = ['All', 'Orientation 2026', 'TechFusion 2025', 'Club moments'];
+export const galleryEvents = ['All', 'Launch Code', 'Orientation 2026', 'TechFusion 2025', 'Club moments'];
+
--- client/src/data/homeSlideshow.js
+++ client/src/data/homeSlideshow.js
@@ -1,5 +1,12 @@
 // Complete list of all images from /images/gallery/ and /images/gallery2/
 export const homeSlideshowImages = [
+  // Latest / Featured: Launch Code
+  { src: '/images/gallery/IMG_20260905_142230.webp', title: 'Launch Code Contest', tag: 'Launch Code' },
+  { src: '/images/gallery/IMG_20260829_142534.webp', title: 'Launch Code Problem Solving', tag: 'Launch Code' },
+  { src: '/images/gallery/IMG_20260829_141921.webp', title: 'Launch Code Coding Session', tag: 'Launch Code' },
+  { src: '/images/gallery/IMG-20260829-WA0011.webp', title: 'Launch Code Participants', tag: 'Launch Code' },
+  { src: '/images/gallery/IMG-20260829-WA0012.webp', title: 'Launch Code Live Arena', tag: 'Launch Code' },
+
   // Gallery 1 (29 images)
   { src: '/images/gallery/1.webp', title: 'Orientation & Welcome', tag: 'Orientation' },
   { src: '/images/gallery/2.webp', title: 'Hands-on Workshop', tag: 'Workshop' },
@@ -30,11 +37,6 @@ export const homeSlideshowImages = [
   { src: '/images/gallery/IMG_0725.webp', title: 'Project Exhibition', tag: 'Exhibition' },
   { src: '/images/gallery/IMG_0729.webp', title: 'Closing Ceremonies', tag: 'Awards' },
   { src: '/images/gallery/IMG_0730.webp', title: 'Group Victory Celebration', tag: 'Celebration' },
-  { src: '/images/gallery/IMG_20260905_142230.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG_20260829_142534.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG_20260829_141921.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG-20260829-WA0011.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG-20260829-WA0012.webp', title: 'Launch Code', tag: '' },
 
   // Gallery 2 (13 images)
   { src: '/images/gallery2/1.webp', title: 'Club Core Moments', tag: 'Culture' },
@@ -50,9 +52,9 @@ export const homeSlideshowImages = [
   { src: '/images/gallery2/11.webp', title: 'Hackathon Final Review', tag: 'Review' },
   { src: '/images/gallery2/13.webp', title: 'Community Celebrations', tag: 'Moments' },
   { src: '/images/gallery2/14.webp', title: 'The Next Generation of Builders', tag: 'NIT Srinagar' },
-  { src: '/images/gallery/IMG_20260905_142230.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG_20260829_142534.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG_20260829_141921.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG-20260829-WA0011.webp', title: 'Launch Code', tag: '' },
-  { src: '/images/gallery/IMG-20260829-WA0012.webp', title: 'Launch Code', tag: '' },
+  { src: '/images/gallery2/IMG_20260905_142230.webp', title: 'Launch Code Contest', tag: 'Launch Code' },
+  { src: '/images/gallery2/IMG_20260829_142534.webp', title: 'Launch Code Problem Solving', tag: 'Launch Code' },
+  { src: '/images/gallery2/IMG_20260829_141921.webp', title: 'Launch Code Coding Session', tag: 'Launch Code' },
+  { src: '/images/gallery2/IMG-20260829-WA0011.webp', title: 'Launch Code Participants', tag: 'Launch Code' },
+  { src: '/images/gallery2/IMG-20260829-WA0012.webp', title: 'Launch Code Live Arena', tag: 'Launch Code' },
 ];
